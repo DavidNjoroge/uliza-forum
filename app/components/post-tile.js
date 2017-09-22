c02f43d133d04e141dc360c6cf4216baab0f4e37
njorogedavid13@gmail.com <njorogedavid13@gmail.com>
2017-09-22 12:52:05 +0300

    add feature: the delete button for removing posts

--- app/components/post-tile.js
+++ app/components/post-tile.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    delete(post) {
+      if (confirm('are you sure you want to delete the post')) {
+        this.sendAction('destroyPost', post)
+      }
+    }
+  }
 });
